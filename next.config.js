a9b8fde67244f9fb4c7608fdc30e8feea0d420f9
keguigong <keguigong@gmail.com>
2023-11-18 19:19:13 +0800

    Add basePrefix and assetPrefix to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const basePath = process.env.NODE_ENV === "production" ? "/chrome-dino" : ""
+const assetPrefix = process.env.NODE_ENV === "production" ? "/chrome-dino" : "/"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath,
+  assetPrefix
 }
 
 module.exports = nextConfig
